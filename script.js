3465ecda2fb8c971f6d2b2c55b81ca5bfd70059d
Amel252 <amelghanem25@gmail.com>
2025-05-25 17:15:12 +0200

    creation js file

---
+++ script.js
@@ -0,0 +1,12 @@
+const imageSpanEl = document.querySelector("image-container");
+const prevEl = document.getElementById("prev");
+const nxtEl = document.getElementById("next");
+
+prevEl.addEventListener("click", function () {
+    showprevImage();
+    console.log("image precedente");
+});
+
+function showprevImage() {
+    return "hello";
+}
